d30c75a9a7db3cb205b58564fc40bc6e69f276e7
ahmetserdargurbuz <ahmetserdargurbuz@gmail.com>
2019-04-13 03:44:02 +0300

    Text update

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,7 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-
+messageTwo.textContent = "This website was created by Ahmet Serdar Gurbuz"
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
